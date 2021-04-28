a84106186aa407bc5c04477269986527e8e2a4e0
AnnaTayanova <82675003+AnnaTayanova@users.noreply.github.com>
2021-04-28 19:34:05 +0300

    Hierarhy first fix

---
+++ Hierarhy/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hierarhy
+{
+    class Book : Printed_product
+    {
+        public string Author { get; set; }
+        public string Name { get; set; }
+        public Book(int pages, int height, int width, string author, string name)
+        {
+            Pages = pages;
+            Height = height;
+            Width = width;
+            Author = author;
+            Name = name;
+        }
+        public override void GetInfo()
+        {
+            Console.WriteLine("\nКнига с названием {0} автора {1} размером {2} на {3} имеет {4} страниц(ы)", Name, Author, Height, Width, Pages);
+        }
+    }
+}
---
+++ Hierarhy/Printed_product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hierarhy
+{
+    abstract class Printed_product
+    {
+        public int Pages { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public abstract void GetInfo();
+    }
+}
