e7b3cb9da92ecacc06a0a5a86afc324048474f14
AnnaTayanova <82675003+AnnaTayanova@users.noreply.github.com>
2021-05-07 20:38:37 +0300

    Tests 1.0

---
+++ GeometricFigures1/TestRectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using NUnit.Framework;
+
+namespace GeometricFigures1
+{
+    public class TestRectangle
+    {
+
+        [Test]
+        public void TestArea([Range(0, 20)] int x, [Range(0, 20)] int y)
+        {
+            var area = GeometricFigures.Rectangle.GetArea(x, y);
+            var area_formula = x * y;
+            Assert.AreEqual(area_formula, area, $"Ожидается {area_formula}, пришло {area}");
+        }
+
+        //Не выполнены: 39
+    
+        [Test]
+        public void TestLength([Range(0, 20)] int x, [Range(0, 20)] int y)
+        {
+            var length = GeometricFigures.Rectangle.GetLength(x, y);
+            var length_formula = (x + y) * 2;
+            Assert.AreEqual(length_formula, length, $"Ожидается {length_formula}, пришло {length}");
+        }
+
+        //Не выполнены: 41
+    }
+}
---
+++ GeometricFigures1/TestSquare.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using NUnit.Framework;
+
+namespace GeometricFigures1
+{
+    class TestSquare
+    {
+        [Test]
+        public void TestArea([Range(0, 20)] int x)
+        {
+            var area = GeometricFigures.Square.GetArea(x);
+            var area_formula = x * x;
+            Assert.AreEqual(area_formula, area, $"Ожидается {area_formula}, пришло {area}");
+        }
+
+        //Не выполнены: 2
+
+        [Test]
+        public void TestLength([Range(0, 20)] int x)
+        {
+            var length = GeometricFigures.Square.GetLength(x);
+            var length_formula = x * 4;
+            Assert.AreEqual(length_formula, length, $"Ожидается {length_formula}, пришло {length}");
+        }
+
+        //Не выполнены: 2
+    }
+}
